99b2a4e85783eddd830ae8a1cbbb34b37b714660
minjun Kim <gofiction95@gmail.com>
2022-11-18 00:20:40 +0900

    회원가입에 nickname 추가완료

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.core.validators import MinValueValidator
 
 
 # Create your models here.
 
 class User(AbstractUser):
-    nickname = models.CharField(null=True, max_length=30)
-    profile = models.TextField()
+    nickname = models.CharField(max_length=30,null=True)
+    # profile_image = ProcessedImageField(
+    #     blank=True,
+    #     upload_to='profile_image/%Y/%m',
+    #     processors=[ResizeToFill(300, 300)],
+    #     format='JPEG',
+    #     options={'quality': 70},
+    # )
     followings = models.ManyToManyField("self", symmetrical=False,related_name='followers')
 
