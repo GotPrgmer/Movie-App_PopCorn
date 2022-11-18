20744a5d1c215c908f2434bf4931f3a7868f5bce
minjun Kim <gofiction95@gmail.com>
2022-11-19 04:45:14 +0900

    11/19 4am 자기전

--- backend/final_pjt_back/urls.py
+++ backend/final_pjt_back/urls.py
@@ -18,11 +18,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('articles.urls')),
+    # path('api/v1/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
     path('movies/', include('movies.urls')),
     # path('accounts/', include('accounts.urls')),
+    path('articles/', include('articles.urls')),
     # path('community/', include('community.urls')),
 ]
