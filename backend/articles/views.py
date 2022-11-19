4b4a60526378489b761e4a6d0a3262e43664e2e5
minjun Kim <gofiction95@gmail.com>
2022-11-19 23:15:17 +0900

    like completed

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -10,6 +10,7 @@ from rest_framework.decorators import api_view
 # permission Decorators
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
 
 from rest_framework import status
 from django.shortcuts import get_object_or_404, get_list_or_404
@@ -30,6 +31,34 @@ def onepersonlike(request,username):
     user = User.objects.get(username=username)
     serializer = UserLikeSerializer(user)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def clicklike(request,user_id,review_id):
+    user = User.objects.get(pk=user_id)
+    review = Review.objects.get(id=review_id)
+
+    if review.like_users.filter(pk=user_id).exists():
+        review.like_users.remove(user)
+        is_liked = False
+    else:
+        review.like_users.add(user)
+        is_liked = True
+    users = review.like_users.all()
+    likepeople = []
+    for e in users:
+        likepeople.append(e.username)
+    print(len(users))
+    # for user in review.like_users.id:
+    # for e in users:
+    #     print(e,1)
+    # print(len(users))
+    context = {
+        'is_liked':is_liked,
+        'likepeople': likepeople,
+        'likecnt': review.like_users.count()
+    }
+    return JsonResponse(context)
+
 
 
 
